715360ff0a5683559a2ebe20e71e052784a98d42
SHIVAM KUMAR GUPTA <120335161+shivamgt821@users.noreply.github.com>
2023-04-24 10:26:10 +0530

    Create OnetoHundred.java

---
+++ OnetoHundred.java
@@ -0,0 +1,14 @@
+package com.shivam_code.pack;
+
+public class OnetoHundred {
+
+	public static void main(String[] args) {
+		int one='a'/'a';
+		//String s="...*..6.%. ";
+		for(int i=one;i<='d';i++)
+		//for(int i=one;i<=s.length()*s.length();i++)
+		{
+			System.out.println(i);
+		}
+	}
+}

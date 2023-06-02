01aacea0886ad3ad2e8ae0c5487c924980271c5b
SHIVAM KUMAR GUPTA <120335161+shivamgt821@users.noreply.github.com>
2023-06-02 21:27:33 +0530

    Create Dowhile_assignment.java

---
+++ Dowhile_assignment.java
@@ -0,0 +1,19 @@
+public class Dowhile_assignment 
+{
+
+	public static void main(String[] args) 
+	{
+
+		int i = 1;
+		int j = 10;
+		do {
+			if(i++ > j--) 
+			 {
+				continue;
+			 }
+		   }
+		while (i<5); 
+			System.out.println(i+ " " +j);
+	}
+
+}

11ebcdf1e98d5ddb1b45a95b6fe72d785ba77040
SHIVAM KUMAR GUPTA <120335161+shivamgt821@users.noreply.github.com>
2023-07-16 19:47:37 +0530

    Create CelsiustoFahrenheit.java

---
+++ CelsiustoFahrenheit.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class CelsiustoFahrenheit {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Enter temp in Celsius : ");
+		Scanner scn = new Scanner(System.in);
+		float temperature = scn.nextFloat();
+		
+		temperature = (temperature*9/5)+32;
+		
+		System.out.println("The temperature in Farhenheit "+temperature);
+	}
+
+}
